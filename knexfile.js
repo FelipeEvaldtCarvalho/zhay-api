1f2830d557fb1023368c8be2cf46f7a0473071ee
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2023-08-25 18:54:59 -0300

    ajustes de bd

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports = {
   client: "mysql2",
   connection: {
+    host: process.env.DATABASE_HOST,
     database: process.env.DATABASE_NAME,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASS,
--- src/database/migrations/20230822234838_initial_migrations.js
+++ src/database/migrations/20230822234838_initial_migrations.js
@@ -6,48 +6,55 @@ exports.up = (knex) => {
   return knex.schema
     .createTable("users", (table) => {
       table.increments("id").primary();
-      table.string("name");
-      table.string("email");
-      table.string("password_hash", 64);
-      table.string("role");
-      table.bigInteger("cpf").nullable();
+      table.string("name").notNullable();
+      table.string("email").notNullable().unique();
+      table.string("password_hash", 64).notNullable();
+      table.string("role").notNullable();
+      table.bigInteger("cpf").nullable().unique();
       table.string("phone").nullable();
     })
     .createTable("addresses", (table) => {
       table.increments("id").primary();
-      table.integer("user_id").unsigned().references("id").inTable("users");
-      table.string("street");
-      table.string("city");
-      table.string("state");
-      table.bigInteger("cep");
-      table.boolean("main").defaultTo(false);
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .notNullable();
+      table.string("street").notNullable();
+      table.string("city").notNullable();
+      table.string("state").notNullable();
+      table.bigInteger("cep").notNullable();
+      table.boolean("main").defaultTo(false).notNullable();
     })
     .createTable("categories", (table) => {
       table.increments("id").primary();
-      table.string("name");
+      table.string("name").notNullable().unique();
     })
     .createTable("products", (table) => {
       table.increments("id").primary();
-      table.string("name");
+      table.string("name").notNullable();
       table.string("description");
-      table.decimal("value", 10, 2);
+      table.decimal("value", 10, 2).notNullable();
     })
     .createTable("products_categories", (table) => {
       table
         .integer("product_id")
         .unsigned()
         .references("id")
-        .inTable("products");
+        .inTable("products")
+        .notNullable();
       table
         .integer("category_id")
         .unsigned()
         .references("id")
-        .inTable("categories");
+        .inTable("categories")
+        .notNullable();
       table.primary(["product_id", "category_id"]);
     })
     .createTable("variants", (table) => {
       table.increments("id").primary();
-      table.string("name");
+      table.string("name").notNullable();
     })
     .createTable("variant_product", (table) => {
       table.increments("id").primary();
@@ -55,62 +62,89 @@ exports.up = (knex) => {
         .integer("product_id")
         .unsigned()
         .references("id")
-        .inTable("products");
+        .inTable("products")
+        .notNullable();
       table
         .integer("variant_id")
         .unsigned()
         .references("id")
-        .inTable("variants");
+        .inTable("variants")
+        .notNullable();
       table.primary(["product_id", "variant_id"]);
     })
     .createTable("carts", (table) => {
       table.increments("id").primary();
-      table.integer("user_id").unsigned().references("id").inTable("users");
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .notNullable();
     })
     .createTable("cart_items", (table) => {
       table.increments("id").primary();
-      table.integer("cart_id").unsigned().references("id").inTable("carts");
+      table
+        .integer("cart_id")
+        .unsigned()
+        .references("id")
+        .inTable("carts")
+        .notNullable();
       table
         .integer("product_id")
         .unsigned()
         .references("id")
-        .inTable("products");
+        .inTable("products")
+        .notNullable();
       table
         .integer("variant_id")
         .unsigned()
         .references("id")
-        .inTable("variants");
-      table.integer("quantity").notNullable();
+        .inTable("variants")
+        .notNullable();
+      table.integer("quantity").notNullable().notNullable();
     })
     .createTable("orders", (table) => {
       table.increments("id").primary();
-      table.integer("user_id").unsigned().references("id").inTable("users");
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .notNullable();
       table
         .integer("address_id")
         .unsigned()
         .references("id")
-        .inTable("addresses");
-      table.string("status");
+        .inTable("addresses")
+        .notNullable();
+      table.string("status").notNullable();
     })
     .createTable("order_statuses", (table) => {
       table.increments("id").primary();
-      table.string("status_name");
+      table.string("status_name").notNullable();
     })
     .createTable("order_details", (table) => {
       table.increments("id").primary();
-      table.integer("order_id").unsigned().references("id").inTable("orders");
+      table
+        .integer("order_id")
+        .unsigned()
+        .references("id")
+        .inTable("orders")
+        .notNullable();
       table
         .integer("product_id")
         .unsigned()
         .references("id")
-        .inTable("products");
+        .inTable("products")
+        .notNullable();
       table
         .integer("variant_id")
         .unsigned()
         .references("id")
-        .inTable("variants");
-      table.integer("quantity").notNullable();
-      table.decimal("value", 10, 2);
+        .inTable("variants")
+        .notNullable();
+      table.integer("quantity").notNullable().notNullable();
+      table.decimal("value", 10, 2).notNullable();
     })
     .createTable("ratings", (table) => {
       table.increments("id").primary();
@@ -118,9 +152,15 @@ exports.up = (knex) => {
         .integer("product_id")
         .unsigned()
         .references("id")
-        .inTable("products");
-      table.integer("user_id").unsigned().references("id").inTable("users");
-      table.integer("rating");
+        .inTable("products")
+        .notNullable();
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .notNullable();
+      table.integer("rating").notNullable();
       table.string("comment");
     });
 };
@@ -131,17 +171,17 @@ exports.up = (knex) => {
  */
 exports.down = (knex) => {
   return knex.schema
-    .dropTable("users")
+    .dropTable("cart_items")
+    .dropTable("carts")
+    .dropTable("order_details")
+    .dropTable("orders")
     .dropTable("addresses")
+    .dropTable("users")
     .dropTable("categories")
     .dropTable("products")
     .dropTable("products_categories")
     .dropTable("variants")
     .dropTable("variant_product")
-    .dropTable("carts")
-    .dropTable("cart_items")
-    .dropTable("orders")
     .dropTable("order_statuses")
-    .dropTable("order_details")
     .dropTable("ratings");
 };
