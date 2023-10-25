c03762c5c094df30b64a3888c27583f47b5b1f02
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2023-10-25 08:40:12 -0300

    rota order-detail para testes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const OrderController = require("../controllers/orderController");
 
 router.get("/", (req, res) => {
   res.send("Hell on World!!!");
+});
+
+router.get("/order-detail/:id", async (req, res) => {
+  const tigas = await OrderController.getOrderDetails(req.body.id);
+  res.send(tigas);
 });
 
 module.exports = router;
