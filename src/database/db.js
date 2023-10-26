8f02776152895c4e3924bcad5c158b73128a6829
Felipe Evaldt Carvalho <felipe.carvalho@appmax.com.br>
2023-10-26 01:53:52 -0300

    add knex paginate

--- src/database/db.js
+++ src/database/db.js
@@ -1,4 +1,6 @@
 const knexfile = require("../../knexfile.js");
 const knex = require("knex")(knexfile);
+const { attachPaginate } = require("knex-paginate");
+attachPaginate();
 
 module.exports = knex;
